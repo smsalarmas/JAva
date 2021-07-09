3ae46de884a50619ae7a793b78824b6ba5eea965
dibrahim <dibra97@gmil.com>
2021-07-09 14:20:22 -0400

    n java

---
+++ config/config.java
@@ -0,0 +1,19 @@
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.jsom.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class config {
+    
+    public static void main (String[] args){
+
+
+
+   }
+}
